bbb84793299cdb5a320a7e7ba421f8a88e71a1e2
parashuram-patil <parasharam_patil@ymail.com>
2020-01-14 13:58:02 +0530

    reading file from class path

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,34 @@
 package hello;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
-  }
+	public static void main(String[] args) throws IOException {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+
+		readFile();
+	}
+
+	private static void readFile() throws IOException {
+		System.out.println("\n ************ Reading File ************ ");
+		InputStream stream = HelloWorld.class.getResourceAsStream("/source.txt");
+        
+		/*System.out.println(stream != null);
+        stream = HelloWorld.class.getClassLoader().getResourceAsStream("source.txt");
+        System.out.println(stream != null);*/
+		
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(stream, writer, "UTF-8");
+		System.out.println("Content ---> " + writer.toString());
+		
+	}
 }
